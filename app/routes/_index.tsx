fe0a49a1c7a0d43df174b802a87feac017b7f462
xHomu <84349818+xHomu@users.noreply.github.com>
2023-10-17 15:40:53 -0700

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
+    <div className="font-sans text-lg">
       <h1>Welcome to Remix</h1>
       <ul className="list-disc">
         <li>
