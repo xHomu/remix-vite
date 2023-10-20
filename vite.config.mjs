f2fc6fe534e1fcfd1689a4de79aace4607da36cc
xHomu <84349818+xHomu@users.noreply.github.com>
2023-10-20 10:58:28 -0700

    add million

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,14 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import million from "million/compiler";
 
 export default defineConfig({
   optimizeDeps: {
     include: ["react", "react-dom/client"],
   },
   plugins: [
+    million.vite({ auto: true }),
     remix({
       ignoredRouteFiles: ["**/.*"],
       tailwind: true,
